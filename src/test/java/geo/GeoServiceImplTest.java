b3f7e643afbb4782fa1d4c75b5c5aa52cdd2933e
Drabkin Sergey <dsergeyg85@icloud.com>
2022-07-21 07:53:02 +0300

    third commit

--- src/test/java/geo/GeoServiceImplTest.java
+++ src/test/java/geo/GeoServiceImplTest.java
@@ -3,9 +3,8 @@ package geo;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoServiceImpl;
@@ -21,23 +20,9 @@ public class GeoServiceImplTest {
 
     /*======Можно было бы для удобства в классе Location переопределеить equal======*/
     @ParameterizedTest
-    @ValueSource (strings = {"127.0.0.1", "172.0.32.11", "96.44.183.149"})
-    public void byIpTest(String argument) {
+    @CsvSource({"172.0.32.11, RUSSIA", "96.44.183.149, USA"})
+    public void byIpTest(String argument, String expected) {
         GeoServiceImpl geoService = new GeoServiceImpl();
-        Location location;
-        switch (argument) {
-            case "172.0.32.11":
-                location = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
-                Assertions.assertEquals(location.toString(), geoService.byIp(argument).toString());
-                break;
-            case "96.44.183.149":
-                location = new Location("New York", Country.USA, " 10th Avenue", 32);
-                Assertions.assertEquals(location.toString(), geoService.byIp(argument).toString());
-                break;
-            default:
-                location = new Location(null, null, null, 0);
-                Assertions.assertEquals(location.toString(), geoService.byIp(argument).toString());
-                break;
-        }
+        Assertions.assertEquals(expected, geoService.byIp(argument).getCountry().toString());
     }
 }
